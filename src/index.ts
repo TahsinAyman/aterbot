e69e605a24e55775a69a746a816a64cf5951879b
Tahsin Ayman <87863787+TahsinAyman@users.noreply.github.com>
2025-07-17 04:01:51 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import HTTP from 'http';
 import initBot from "./bot.js";
 
 
+
 const PORT = process.PORT || 5500;
 const server = HTTP.createServer((request, response) => {
 	response.writeHead(200, {
@@ -14,5 +15,9 @@ const server = HTTP.createServer((request, response) => {
 
 
 initBot();
+
+server.listen(PORT, () => {
+	console.log(`AFKBot is running on port ${PORT}`);
+})
 
 export default server;
