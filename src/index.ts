31031e1b9bc961495029af0e7b265e93feedce25
Tahsin Ayman <87863787+TahsinAyman@users.noreply.github.com>
2025-07-17 04:00:17 +0000

    init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
-import initBot from "./bot.ts";
-import initWeb from "./web.ts";
+import HTTP from 'http';
+import initBot from "./bot.js";
+
+
+const PORT = process.PORT || 5500;
+const server = HTTP.createServer((request, response) => {
+	response.writeHead(200, {
+		"Access-Control-Allow-Origin": "https://replit.com",
+		"Access-Control-Allow-Methods": "GET, PING, OPTIONS",
+		"Content-Type": "text/html"
+	} as const);
+	response.end("<h3>Copy me, the url above!</h3>");
+});
+
 
 initBot();
-initWeb();
+
+export default server;
